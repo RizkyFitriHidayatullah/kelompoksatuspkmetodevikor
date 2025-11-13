778c14b563bda04dd8617c006e4bb88f3d6a2a6f
RizkyFitriHidayatullah <rizkyfitrihidayatullah@gmail.com>
2025-11-13 20:04:49 +0700

    Update vikor_streamlit.py

--- vikor_streamlit.py
+++ vikor_streamlit.py
@@ -61,8 +61,8 @@ def vikor(decision_matrix, weights, criterion_types, v=0.5):
 # STREAMLIT UI
 # ---------------------------------------
 st.set_page_config(page_title="Sistem Pendukung Keputusan - VIKOR", layout="centered")
-st.title("💡 Sistem Pendukung Keputusan rekomendasi laptop terbaik untuk mahasiswa menggunakan Metode VIKOR")
-st.write("Gunakan aplikasi ini untuk menghitung hasil ranking alternatif berdasarkan metode **VIKOR**.")
+st.title("💡 Sistem Pendukung Keputusan Rekomendasi Laptop Terbaik untuk Mahasiswa")
+st.write("Gunakan aplikasi ini untuk menentukan alternatif laptop terbaik berdasarkan metode **VIKOR**.")
 
 st.divider()
 
@@ -123,4 +123,4 @@ if m and n:
         )
 
 st.divider()
-st.caption("Dibuat oleh kelompok 1 SPK STT Wastukancana Purwakarta menggunakan Streamlit & Python | Metode VIKOR")
+st.caption("Dibuat oleh Kelompok 1 SPK STT Wastukancana Purwakarta menggunakan Streamlit & Python | Metode VIKOR")
